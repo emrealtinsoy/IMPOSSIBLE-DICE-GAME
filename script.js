ce954a63c054479ba09ed7fb5d1cb21ae2a1466a
emrealtinsoy <131373441+emrealtinsoy@users.noreply.github.com>
2023-06-12 22:31:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 const dice = document.getElementById('dice-btn');
-
-function diceGame() {
-
-	var randomDice = Math.floor((Math.random() * 6) + 1);
-	var diceLocation = "img/" + "Dice-" + randomDice + ".png";
-	var img1 = document.querySelectorAll("img")[0];
-	img1.setAttribute("src", diceLocation);
+function diceGame(){
+var a = Math.random();
+	if(a < 0.990){
+		var randomDice = Math.floor((Math.random() * 6) + 1);
+		var diceLocation = "img/" + "Dice-" + randomDice + ".png";
+		var img1 = document.querySelectorAll("img")[0];
+		img1.setAttribute("src", diceLocation);
 	
-	var randomDice2 = randomDice + Math.floor((Math.random() * (6-randomDice)));
-	var diceLocation2 = "img/" + "Dice-" + randomDice2 + ".png";
-	var img2 = document.querySelectorAll("img")[1];
-	img2.setAttribute("src", diceLocation2);
-
-	if (randomDice > randomDice2) {
+		var randomDice2 = randomDice + Math.floor((Math.random() * (6-randomDice)) + 1) - 1;
+		var diceLocation2 = "img/" + "Dice-" + randomDice2 + ".png";
+		var img2 = document.querySelectorAll("img")[1];
+		img2.setAttribute("src", diceLocation2);
+	}	
+	else{
+		var randomDice = Math.floor((Math.random() * 5) + 1) + 1;
+		var diceLocation = "img/" + "Dice-" + randomDice + ".png";
+		var img1 = document.querySelectorAll("img")[0];
+		img1.setAttribute("src", diceLocation);
+		
+		var randomDice2 = 1;
+		var diceLocation2 = "img/" + "Dice-" + randomDice2 + ".png";
+		var img2 = document.querySelectorAll("img")[1];
+		img2.setAttribute("src", diceLocation2);
+	}
+	if(randomDice > randomDice2){
 		document.querySelector("h1").innerHTML = "YOU WIN !";
 	}
-	else if (randomDice < randomDice2) {
+	else if(randomDice < randomDice2){
 		document.querySelector("h1").innerHTML = "COMPUTER WIN";
 	}
-	else if (randomDice = randomDice2) {
+	else if(randomDice = randomDice2){
 		document.querySelector("h1").innerHTML = "DRAW !";
 	}
-
-	dice.innerHTML = "PLAY AGAIN"
+dice.innerHTML = "PLAY AGAIN"
 }
